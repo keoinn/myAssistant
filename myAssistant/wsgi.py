be4fd43cd52932f7be45be1af693769157e149c8
Ken Chang <keoinn@gmail.com>
2023-09-06 00:13:28 +0800

    fix wsgi path

--- myAssistant/wsgi.py
+++ myAssistant/wsgi.py
@@ -12,7 +12,10 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = "/home/keoinn/myAssistant/"
+path = "/home/keoinn/django_env/lib/python3.10/site-packages"
+if path not in sys.path:
+    sys.path.insert(0, path)
+path = "/home/keoinn/myAssistant"
 if path not in sys.path:
     sys.path.insert(0, path)
 
