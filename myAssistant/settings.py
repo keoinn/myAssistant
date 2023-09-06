bd6dfd26fc2f8d943f8ca49738441809dfed75a6
Ken Chang <keoinn@gmail.com>
2023-09-07 04:18:47 +0800

    fix static settings

--- myAssistant/settings.py
+++ myAssistant/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os # Line 120
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
